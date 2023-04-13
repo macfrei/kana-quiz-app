db3dece8db5980e12da6977200dc5243fe15933e
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-04-13 16:52:31 +0900

    Extract fieldest with radio buttons to its own component

--- src/components/KanaQuiz.tsx
+++ src/components/KanaQuiz.tsx
@@ -2,6 +2,7 @@ import { type KanaType } from '../types/kana';
 import useQuizForm from '../hooks/useQuizForm';
 import React from 'react';
 import styled from 'styled-components';
+import Question from './Question';
 
 type KanaQuizProps = {
   kana: KanaType[];
@@ -25,21 +26,11 @@ export default function KanaQuiz({ kana }: KanaQuizProps) {
   return (
     <Form aria-labelledby="form-headline" onSubmit={handleSubmit}>
       <Headline id="form-headline">Kana Quiz</Headline>
-      <Fieldset>
-        <Legend>{quizQuestion.question.kana}</Legend>
-        {quizQuestion.answers.map(answer => (
-          <RadioButtonGroup key={answer.id}>
-            <Input
-              id={answer.id}
-              type="radio"
-              name={quizQuestion.question.kana}
-              value={answer.kana}
-              onChange={() => checkAnswer(answer)}
-            />
-            <Label htmlFor={answer.id}>{answer.pronunciation}</Label>
-          </RadioButtonGroup>
-        ))}
-      </Fieldset>
+      <Question
+        question={quizQuestion.question}
+        answers={quizQuestion.answers}
+        onChange={checkAnswer}
+      />
       {quizKanaLength > 1 && (
         <Button
           type="button"
@@ -73,52 +64,6 @@ const Headline = styled.h2`
   font-weight: normal;
   margin: 0;
   padding: 0;
-`;
-
-const Fieldset = styled.fieldset`
-  display: grid;
-  place-items: center;
-  grid-template-columns: 1fr 1fr;
-  gap: 12px;
-  border: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const RadioButtonGroup = styled.div`
-  display: grid;
-  place-items: center;
-  position: relative;
-  width: 100%;
-  height: 100%;
-
-  input[type='radio']:checked + label {
-    background-color: black;
-    color: white;
-  }
-`;
-
-const Legend = styled.legend`
-  font-size: 400%;
-  text-align: center;
-  padding: 40px 0;
-`;
-
-const Label = styled.label`
-  display: grid;
-  place-items: center;
-  border: 1px solid black;
-  border-radius: 8px;
-  width: 100%;
-  height: 100%;
-  font-size: 200%;
-`;
-
-const Input = styled.input`
-  position: absolute;
-  width: 0;
-  height: 0;
-  opacity: 0;
 `;
 
 const Button = styled.button`
